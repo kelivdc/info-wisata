23feaf2e73fb4e8afe7946a1341fd703038c4bfc
Standpoint <kelivdc@yahoo.com>
2022-06-25 20:03:22 +0700

    index page

--- components/Frontend.js
+++ components/Frontend.js
@@ -5,6 +5,7 @@ import { SunIcon } from '@chakra-ui/icons'
 import Head from 'next/head'
 import Navbar from './Navbar'
 import Topbar from './Topbar'
+import Footer from './Footer'
 
 function Frontend({children, metadata}) {  
   const meta = {
@@ -15,18 +16,19 @@ function Frontend({children, metadata}) {
   
   return (
     <>
-    <Head>
-      <title>{meta.title}</title>
-      <meta name="description" content={meta.description} />
-      <meta name="robots" content={meta.robots} />
-      <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
-	    <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
-    </Head>
-    <Container maxW='1040px' p="0">          
-       <Topbar />
-      <Navbar />
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <meta name="robots" content={meta.robots} />
+        <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+        <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+      </Head>
+      <Container maxW='1040px' p="0">          
+        <Topbar />
+        <Navbar />
+      </Container>
       {children}
-    </Container>
+      <Footer />    
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Image, useColorModeValue } from "@chakra-ui/react";
 import Frontend from "../components/Frontend";
 
 export default function Home() {
@@ -7,16 +7,25 @@ export default function Home() {
     description: 'Website paling keren',
     robots: 'index, follow'
   }
-  return (
-    <Frontend metadata={metadata}>      
-      <Flex direction={{base: "column", md: "row"}} gap={{base: 2, md: 8}}>
-        <Box w={{base: "100%", md: "70%"}} bg="yellow.200">
-          Kiri
-        </Box>
-        <Box w={{base: "100%", md: "30%"}} bg="red.200">
-          Kanan
-        </Box>
-      </Flex>
-    </Frontend>       
+    const bgBox = useColorModeValue('gray.100','gray.700')
+    const bgTerbaru = useColorModeValue('gray.600','gray.800')
+  return (    
+      <Frontend metadata={metadata}>        
+        <Box bg={bgBox}>
+          <Container maxW="1040px" p="0">
+          <Flex direction={{base: "column", md: "row"}} gap={{base: 2, md: 8}} p={2} mx="auto">
+            <Box w={{base: "100%", md: "60%"}} position="relative">
+              <Image src="/img/zebra.jpg" rounded={8} />
+              <Box position="absolute" overflow="hidden" top="0" left="0" h="100%" w="100%" bg="#000" opacity="0.4" rounded={8} />
+            </Box>        
+            <Box w={{base: "100%", md: "40%"}}>
+              <Box bg={bgTerbaru} rounded={4} p={4}>
+                <Heading as="h2" fontSize="md" color="white">Info terbaru</Heading>
+              </Box>
+            </Box>
+          </Flex>
+          </Container>
+        </Box>        
+      </Frontend>       
   )
 }
