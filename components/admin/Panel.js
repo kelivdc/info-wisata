cc2ccd7e1af988aaeca5f9d98df466074cc736dc
Standpoint <kelivdc@yahoo.com>
2022-07-09 12:01:20 +0700

    Category create form

--- components/admin/Panel.js
+++ components/admin/Panel.js
@@ -1,7 +1,8 @@
-import { useColorModeValue, Box, Heading } from '@chakra-ui/react'
+import { useColorModeValue, Box, Heading, Flex, Button, Icon, IconButton } from '@chakra-ui/react'
 import React from 'react'
+import { FaPlus } from 'react-icons/fa'
 
-function Panel({children, title}) {
+function Panel({children, title, top_button}) {
     const bgBox = useColorModeValue("white", "gray.300")
     const textColor = useColorModeValue("gray.500", "gray.600")
     const bgPanel = useColorModeValue("white", "gray.600")
@@ -11,7 +12,10 @@ function Panel({children, title}) {
   return (
     <Box bg={bgBox} border={"1px"} borderColor="gray.200" shadow="xl" rounded="md" >
         <Box p={4} bg={bgHeader} roundedTop={"md"} color={textColor} borderBottom={"1px"} borderColor="gray.200">
+          <Flex justifyContent={"space-between"} alignItems="center">
             <Heading fontSize="16px" fontWeight={"bold"} color={textHeader}>{title || ''}</Heading>
+            {top_button}
+          </Flex>
         </Box>
         <Box p={4} bg={bgPanel} color={textPanel} roundedBottom={"md"}>
           {children}
--- pages/admin/category.js
+++ pages/admin/category.js
@@ -1,58 +1,132 @@
-import { Table, TableContainer, Text, Thead, Th, Tr, Tbody, Td, Flex, Box, Input, Select, HStack } from '@chakra-ui/react'
-import React from 'react'
-import Panel from '../../components/admin/Panel'
-import Template from '../../components/admin/Template'
+import { AddIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import {
+  Table,
+  TableContainer,
+  Text,
+  Thead,
+  Th,
+  Tr,
+  Tbody,
+  Td,
+  Flex,
+  Box,
+  Button,
+  Input,
+  Select,
+  HStack,
+  IconButton,
+  Tooltip, 
+  InputGroup,  
+  InputRightElement,
+  FormControl,
+  useDisclosure
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import Create from "../../components/admin/modal/category/Create";
+import Panel from "../../components/admin/Panel";
+import Template from "../../components/admin/Template";
+
+function Search() {
+    console.log('Search')
+}
 
 function Category() {
+    const [message, setMessage] = useState('')    
+    const modal_create = useDisclosure()
+    const top_button = (
+        <Button onClick={modal_create.onOpen} size="sm" colorScheme={"green"} leftIcon={<AddIcon />}>Create</Button>
+    )
+  
+    const submit = (event) => {
+        if (event.keyCode == 13) {
+            Search();
+        }
+    }
   return (
     <Template title="Category">
-      <Panel title="Category">
+      <Create modal_name={modal_create} />
+      <Panel title="Category" top_button={top_button}>
+        <Box my={4}>
+            {message}
+        </Box>
         <Flex justifyContent={"space-between"}>
-            <HStack>
-                <Text>Search</Text> <Input type="text" size="sm" placeholder='keyword' rounded={"sm"} />
-            </HStack>
-            <Box>A</Box>
+          <HStack>
+            <Text>Search</Text>
+            <FormControl>
+                <InputGroup size="md">
+                <Input type="text" placeholder="keyword" rounded={"sm"} onKeyDown={(e) => submit(e)} />
+                <InputRightElement>
+                    <IconButton size="sm" icon={<FaSearch />} onClick={Search()}/>
+                </InputRightElement>
+                </InputGroup>
+            </FormControl>
+          </HStack>
+          <Box>Total: 25 entries</Box>
         </Flex>
         <TableContainer my={4}>
-            <Table variant="striped" border={"1px"} borderColor="gray.200">
-                <Thead borderBottom={"2px"} borderColor={"gray.300"}>
-                    <Tr>
-                        <Th>Id</Th>
-                        <Th>Name</Th>
-                        <Th>Slug</Th>
-                        <Th>Sort</Th>
-                        <Th>Publish</Th>
-                        <Th>Action</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                {[...Array(10)].map((x, i) =>                       
-                    <Tr>
-                        <Td>1</Td>
-                        <Td>Wisata</Td>
-                        <Td>wisata</Td>
-                        <Td>10</Td>
-                        <Td>True</Td>
-                        <Td>[Edit] [Delete]</Td>
-                    </Tr>                
-                )}
-                </Tbody>
-            </Table>
+          <Table
+            variant="striped"
+            border={"1px"}
+            borderColor="gray.200"
+            size="sm"
+            p={0}
+          >
+            <Thead borderBottom={"2px"} borderColor={"gray.300"}>
+              <Tr>
+                <Th w="1%" py={2}>Id</Th>
+                <Th>Name</Th>
+                <Th>Slug</Th>
+                <Th>Sort</Th>
+                <Th>Publish</Th>
+                <Th textAlign={"center"}>Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {[...Array(10)].map((x, i) => (
+                <Tr key={i} py="0" size="sm">
+                  <Td>{i}</Td>
+                  <Td>Wisata</Td>
+                  <Td>wisata</Td>
+                  <Td>10</Td>
+                  <Td>True</Td>
+                  <Td textAlign={"center"}>
+                    <Tooltip label="Edit">
+                      <IconButton
+                        icon={<EditIcon />}
+                        size="sm"
+                        colorScheme="green"
+                      />
+                    </Tooltip>
+                    <Tooltip label="Delete">
+                      <IconButton
+                        ml="5px"
+                        icon={<DeleteIcon />}
+                        size="sm"
+                        colorScheme="red"
+                      />
+                    </Tooltip>               
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
         </TableContainer>
         <Flex justifyContent={"space-between"}>
-            <HStack>
-            <Box>Show</Box> <Select w="80px">
-                <option value="10">10</option>
-                <option value="10">25</option>
-                <option value="10">50</option>
+          <HStack>
+            <Box>Show</Box>{" "}
+            <Select w="80px">
+              <option value="10">10</option>
+              <option value="10">25</option>
+              <option value="10">50</option>
             </Select>
             <Box>entries</Box>
-            </HStack>
-            <Box>Pagination</Box>
+          </HStack>
+          <Box>Pagination</Box>
         </Flex>
       </Panel>
     </Template>
-  )
+  );
 }
 
-export default Category
+export default Category;
